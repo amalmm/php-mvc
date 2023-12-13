17aeb2018075ccb10dd6c010b10580224ed7b49f
amal sienti <amalramachandran8@gmail.com>
2023-12-13 17:56:34 +0530

    fixed index / route and

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -14,7 +14,7 @@ class IndexController {
             $email = isset( $_GET['email']) ? $_GET['email'] : null ;
             if (empty($email)) {
                  $_SESSION['error']['message'] =  "error : message name field required" ;
-                header('Location:/index?error=email requierd ');
+                header('Location:/index');
                 exit();
               }
     }
--- src/View/alert.php
+++ src/View/alert.php
@@ -3,3 +3,5 @@
 if (isset($_SESSION['error'])) {
     echo isset($_SESSION['error']['message']) ? $_SESSION['error']['message'] : '';
 }
+
+unset($_SESSION['error']);
